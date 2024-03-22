f76bd3f2da5134af638984953fe8e9216b0766f5
Dominik Naumann <dominik.naumann@yahoo.de>
2024-03-22 16:56:52 +0100

    all done

--- risk_icu/ml_flow.py
+++ risk_icu/ml_flow.py
@@ -2,14 +2,19 @@ import pandas as pd
 from risk_icu.model import model_h1, save_model
 from risk_icu.preprocessing import preprocessing_1_hour
 
-#data
-data = pd.read_csv("raw_data/training_v2.csv")
+def main():
+    #data
+    data = pd.read_csv("raw_data/training_v2.csv")
 
-# preproc
-X, y = preprocessing_1_hour(data)
+    # preproc
+    X, y = preprocessing_1_hour(data)
 
-# training
-model, auc_score, recall = model_h1(X,y)
+    # training
+    model, auc_score, recall = model_h1(X,y)
 
-#save model
-save_model(model)
+    #save model
+    save_model(model)
+
+
+if __name__ == "__main__":
+    main()
